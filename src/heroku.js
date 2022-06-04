10fb94f8907a301e28ed8ae40f6dd935437fe114
Rosie Jane Enomar <rosiejaneenomar@gmail.com>
2022-06-04 16:37:49 +0800

    updates heroku app config

--- src/heroku.js
+++ src/heroku.js
@@ -1,3 +1,23 @@
+// get environment variables dynamically and create environment.ts file
+const fs = require("fs");
+const targetPath = "./src/environments/environment.ts";
+const envConfigFile = `export const environment = {
+  production: '${process.env.PRODUCTION}',
+  API_KEY: '${process.env.API_KEY}',
+  TAILWIND_MODE: 'build',
+  NODE_ENV: 'production'
+};
+`;
+fs.writeFile(targetPath, envConfigFile, function (err) {
+  if (err) {
+    throw console.error(err);
+  } else {
+    console.log(
+      `Angular environment.ts file generated correctly at ${targetPath} \n`
+    );
+  }
+});
+
 //Install express server
 const express = require('express');
 const path = require('path');
